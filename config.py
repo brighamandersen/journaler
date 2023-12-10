d2d969a9f129d131ee5d6f922a0ead2dd035ef79
Brigham Andersen <brighambandersen@gmail.com>
2023-12-09 22:23:45 -0700

    Add secret key

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ import os
 
 app = Flask(__name__)
 
+app.secret_key = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
     os.path.join(os.path.abspath(os.path.dirname(__file__)), 'db.sqlite')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
--- models.py
+++ models.py
@@ -5,7 +5,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
 #     # password = db.Column(db.String(50), nullable=False)
-#     entries = db.relationship("JournalEntry", backref="author")
 
 
 class JournalEntry(db.Model):
